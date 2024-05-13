f3714f9fd1a50e204dc0ae7e4dd1521078ff72ef
AmirHossein 🍏 <ahosshams@protonmail.com>
2024-05-13 17:17:51 +0330

    installer will install plugins.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+"$SCRIPT_DIR/vim-plug-install.sh"
 cp "$SCRIPT_DIR/.vimrc" ~/.vimrc && echo "\033[32m.vimrc copied to your home directory.\033[32m"
-
+vim -c "PlugInstall | qa!"
